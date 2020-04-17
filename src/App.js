679db7b88404e0cb1507fd41db90a09305e20491
Jose Murillo <jooseemurillo@gmail.com>
2020-04-17 01:29:04 -0300

    UPDARTE

--- src/App.js
+++ src/App.js
@@ -4,33 +4,10 @@ import Header from './components/Header';
 import Feed from './components/Feed';
 
 function App() {
-
-  const [post, setPost] = useState([
-    {
-      id: 1,
-      author: {
-        name: "Julio Alcantara",
-        avatar: "https://conteudo.imguol.com.br/c/entretenimento/4e/2018/08/17/o-musico-gustavo-bertoni-do-scalene-1534530323003_v2_450x450.png"
-      },
-      date: "04 Jun 2019",
-      content: "Pessoal, alguém sabe se a Rocketseat está contratando?",
-      comments: [
-        {
-          id: 1,
-          author: {
-            name: "Diego Fernandes",
-            avatar: "http://url-da-imagem.com/imagem.jpg"
-          },
-          content: "Conteúdo do comentário"
-        }
-      ]
-    }
-  ])
-
   return (
     <div className="App">
       <Header />
-      <Feed posts={post} />
+      <Feed/>
     </div>
   );
 }
--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
-import Post from '../Post'
 
-export default function Feed({ posts }) {
+import api from '../../services/api';
+
+function PostItem({ post }) {
+  
   return (
-    <div className="container">
-      <Post posts={posts} />
+    <div className="container-postitem">
+        <header className="header">
+          <img src={post.author.avatar} />
+          <div className="infors">
+            <span>{post.author.name}</span>
+            <span>{post.date}</span>
+          </div>
+        </header>
+    </div>
+  );
+}
+
+export default function Feed() {
+  const [posts, setPosts] = useState(api);
+
+  return (
+    <div className="container-feed">
+      {posts.map(post => (
+        <PostItem post={post}/>
+      ))}
     </div>
   );
 }
